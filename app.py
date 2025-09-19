2058db5fe4d7bbf0bff142a0f17d1a31ab684cb9
Ananta90-Research <ananta.kumar565@gmail.com>
2025-09-20 03:42:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown("""
 .big-label {
     font-size: 18px;
     font-weight: bold;
-    margin-bottom: 0px;
+    margin-bottom: -2px;
 }
 .smaller-text {
     font-size: 14px;
@@ -160,6 +160,7 @@ with col2:
             st.markdown(f"<p class='smaller-text'>{i}. {val} °C</p>", unsafe_allow_html=True)
     else:
         st.write("No predictions yet.")
+
 
 
 
